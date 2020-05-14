ffe4180db7c6c95f451e772a6ffb94a37f4d80a0
silumankadal <bolehngasal@gmail.com>
2020-05-14 08:31:44 +0700

    first commit

---
+++ Toko bangunan2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Toko_bangunan2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Batako";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Semen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pipa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pasir";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(42, 44);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(42, 126);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(42, 208);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 7;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(42, 293);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Harga batako";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Harga semen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Harga pipa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(259, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Harga pasir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Toko bangunan2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toko_bangunan2
+{
+    public partial class Form1 : Form
+    {
+        class Bangunan
+        {
+            private string Batako;
+            private string Semen;
+            private string Pipa;
+            private string Pasir;
+            private string Total;
+
+            public void setBatako(string val)
+            {
+                this.Batako = val;
+            }
+            public string getBatako()
+            {
+                return Batako;
+            }
+            public void setSemen(string val)
+            {
+                this.Semen = val;
+            }
+            public string getSemen()
+            {
+                return Semen;
+            }
+            public void setPipa(string val)
+            {
+                this.Pipa = val;
+            }
+            public string getPipa()
+            {
+                return Pipa;
+            }
+            public void setPasir(string val)
+            {
+                this.Pasir = val;
+            }
+            public string getPasir()
+            {
+                return Pasir;
+            }
+            public void setTotal(string val)
+            {
+                this.Total = val;
+            }
+            public string getTotal()
+            {
+                return Total;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Bangunan cek = new Bangunan();
+            double a, b, c, d, f, batako, semen, pipa, pasir, total, diskon, harga;
+            a = double.Parse(this.numericUpDown1.Text);
+            b = double.Parse(this.numericUpDown2.Text);
+            c = double.Parse(this.numericUpDown3.Text);
+            d = double.Parse(this.numericUpDown4.Text);
+            f = a + b + c + d;
+            cek.setBatako("Rp50.000 per buah");
+            cek.setSemen("Rp20.000 per kilogram");
+            cek.setPipa("Rp10.000 per buah");
+            cek.setPasir("Rp5.000 per kilogram");
+            if (a + b + c + d > 1)
+            {
+                batako = a * 50000;
+                semen = b * 20000;
+                pipa = c * 10000;
+                pasir = d * 5000;
+                total = batako + semen + pipa + pasir;
+                diskon = 0;
+                for (int i = 1; i <= f; i++)
+                {
+                    if (i % 10 == 0)
+                    {
+                        diskon += 10000;
+                    }
+                    else
+                    {
+                        diskon += 1000;
+                    }
+                }
+                harga = total - diskon;
+                MessageBox.Show("total pembelian " + harga);
+            }
+            else
+            {
+                MessageBox.Show("masukkan yang ingin dibeli");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Bangunan cek = new Bangunan();
+            cek.setBatako("Rp50000 per buah");
+            MessageBox.Show("harga batako " + cek.getBatako());
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Bangunan cek = new Bangunan();
+            cek.setSemen("Rp20000 per kilogram");
+            MessageBox.Show("harga semen " + cek.getSemen());
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Bangunan cek = new Bangunan();
+            cek.setPipa("Rp10000 per buah");
+            MessageBox.Show("harga pipa " + cek.getPipa());
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Bangunan cek = new Bangunan();
+            cek.setPasir("Rp5000 per kilogram");
+            MessageBox.Show("harga pasir " + cek.getPasir());
+        }
+    }
+}
